244cff06cbc4b3970a81339697fe0e25bb1754a4
shivam <shivamkhari6068@gmail.com>
2023-08-06 23:39:21 +0530

    adding heading of the app

--- src/component/Main.js
+++ src/component/Main.js
@@ -6,6 +6,9 @@ function Main() {
         <>
             <div id="maindiv" className="Main">
                 <div id="innerdiv">
+                    <div id="innerdiv1">
+                    <h2 className="heading">Weather Finding App</h2>
+                    </div>
 
                 </div>
             </div>
