d8e97dde3ef905e7f8a308310c14cdfd03a8b469
shivam <shivamkhari6068@gmail.com>
2023-08-06 23:41:42 +0530

    adding input area

--- src/component/Main.js
+++ src/component/Main.js
@@ -7,7 +7,9 @@ function Main() {
             <div id="maindiv" className="Main">
                 <div id="innerdiv">
                     <div id="innerdiv1">
-                    <h2 className="heading">Weather Finding App</h2>
+                        <h2 className="heading">Weather Finding App</h2>
+                        <hr />
+                        <input className="input" type="text" placeholder="Write city name" />
                     </div>
 
                 </div>
